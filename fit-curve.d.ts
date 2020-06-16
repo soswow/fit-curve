e3f44c1eaf4cb335c5ab14dd22997da49783d46b
Anselm Eickhoff <anselm.eickhoff@gmail.com>
2020-06-16 10:27:33 +0200

    Fix inconsitency in JSDoc param naming

--- fit-curve.d.ts
+++ fit-curve.d.ts
@@ -31,7 +31,7 @@ declare module "fit-curve" {
    * @param points - Array of digitized points, e.g. [[5,5],[5,50],[110,140],[210,160],[320,110]]
    * @param leftTangent - Unit tangent vector at start point
    * @param rightTangent - Unit tangent vector at end point
-   * @param error - Tolerance, squared error between points and fitted curve
+   * @param maxError - Tolerance, squared error between points and fitted curve
    * @returns Array of Bezier curves, where each element is [first-point, control-point-1, control-point-2, second-point] and points are [x, y]
    */
   export function fitCubic<Arr extends number[] = [number, number]>(
